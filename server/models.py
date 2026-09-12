875969fa2ea71b68fe56920f1da6e40d16d71cd3
Maurine <maureen.wanjira@gmail.com>
2026-09-12 09:10:16 +0300

    complete backend setup and database migrations

---
+++ server/models.py
@@ -0,0 +1,47 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+
+    notes = db.relationship(
+        "Note",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+
+
+class Note(db.Model):
+    __tablename__ = "notes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
+    )
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    user = db.relationship(
+        "User",
+        back_populates="notes"
+    )
